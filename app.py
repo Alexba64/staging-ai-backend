c23d5342754a21de3478b2976483094729a89110
Alexba64 <info@alexmedia.it>
2025-05-07 17:48:20 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -50,4 +50,5 @@ def upload():
     return jsonify({"error": "Timeout nella generazione dell'immagine"}), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
