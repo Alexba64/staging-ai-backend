9dedcaa8779560dda39e7967044f066450479a75
Alexba64 <info@alexmedia.it>
2025-05-07 19:49:47 +0200

    Update 11 app.py

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import os
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 import replicate
 
 app = Flask(__name__)
+
+# Abilita CORS per tutte le rotte
+CORS(app)
 
 # Inizializzazione del client per l'API di replicate
 REPLICATE_API_TOKEN = os.getenv("REPLICATE_API_TOKEN")
@@ -38,3 +42,4 @@ if __name__ == "__main__":
     # L'app di Flask su Render deve ascoltare sulla porta specificata nell'ambiente
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
